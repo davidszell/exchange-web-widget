2a4e5f6fea44bbc87030052be7016bb94bbb2fdf
David Szell <mail@davidszell.com>
2021-10-25 00:54:59 +0200

    Added direction change tests

--- src/components/Exchange/Exchange.test.tsx
+++ src/components/Exchange/Exchange.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom';
 import 'jest-styled-components';
 import Exchange from './Exchange';
 
@@ -9,5 +11,38 @@ describe('Exchange', () => {
             .create(<Exchange />)
             .toJSON();
         expect(tree).toMatchSnapshot();
-    })
+    });
+
+    it('Shows correct header', () => {
+        render(<Exchange />);
+        expect(screen.getByTestId('header')).toHaveTextContent('Sell');
+    });
+
+    it('Shows correct header after direction change', () => {
+        render(<Exchange />);
+        fireEvent.click(screen.getByTestId('directionButton'));
+        expect(screen.getByTestId('header')).toHaveTextContent('Buy');
+    });
+
+    it('Shows correct direction button label', () => {
+        render(<Exchange />);
+        expect(screen.getByTestId('directionButton')).toHaveTextContent('↓');
+    });
+
+    it('Shows correct direction button after direction change', () => {
+        render(<Exchange />);
+        fireEvent.click(screen.getByTestId('directionButton'));
+        expect(screen.getByTestId('directionButton')).toHaveTextContent('↑');
+    });
+
+    it('Shows correct exchange button label', () => {
+        render(<Exchange />);
+        expect(screen.getByTestId('exchangeButton')).toHaveTextContent('Sell');
+    });
+
+    it('Shows correct exchange button label after direction change', () => {
+        render(<Exchange />);
+        fireEvent.click(screen.getByTestId('directionButton'));
+        expect(screen.getByTestId('exchangeButton')).toHaveTextContent('Buy');
+    });
 })
