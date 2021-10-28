70bba690746d9877c11a3e18347d582a774a3674
David Szell <mail@davidszell.com>
2021-10-28 16:22:04 +0200

    Added setWallets action test

--- src/actions/actions.test.ts
+++ src/actions/actions.test.ts
@@ -1,20 +1,34 @@
-import { setExchangeRates } from "."
+import { setExchangeRates, setWallets } from "."
 
 describe('actions', () => {
-    describe('setExchangeRates', () => {
-        it('should return the correct action', () => {
-            const setExchangeRatesAction = {
-                type: 'SET_EXCHANGE_RATES',
-                payload: {
-                    base: "EUR",
-                    rates: {
-                        "EUR": 1,
-                        "GBP": 0.85,
-                        "USD": 1.16
-                    }
-                }
-            }
-            expect(setExchangeRates(setExchangeRatesAction.payload)).toEqual(setExchangeRatesAction);
-        })
-    })
+  it('setExchangeRates returns the correct action', () => {
+    const setExchangeRatesAction = {
+      type: 'SET_EXCHANGE_RATES',
+      payload: {
+        base: "EUR",
+        rates: {
+          "EUR": 1,
+          "GBP": 0.85,
+          "USD": 1.16
+        }
+      }
+    }
+    expect(setExchangeRates(setExchangeRatesAction.payload)).toEqual(setExchangeRatesAction);
+  })
+
+  it('setWallets returns the correct action', () => {
+    const setWalletsAction = {
+      type: 'SET_WALLETS',
+      payload: [
+        {
+          name: "EUR",
+          balance: 10,
+          longName: "Euros",
+          symbol: "€"
+      }
+      ]
+    }
+
+    expect(setWallets(setWalletsAction.payload)).toEqual(setWalletsAction);
+  })
 })
